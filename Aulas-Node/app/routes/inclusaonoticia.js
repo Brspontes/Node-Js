308c69a16ea107d28acd3120f15175c2c4fc9e5d
Brspontes <brian.robert16@hotmail.com>
2020-12-01 20:51:21 -0300

    Adicionado typescript

--- Aulas-Node/app/routes/inclusaonoticia.js
+++ Aulas-Node/app/routes/inclusaonoticia.js
@@ -5,11 +5,25 @@ module.exports = function(app){
     });
 
     app.post('/noticias/salvar', function(req, res){
+        var noticia = req.body;
+        
+        req.assert('titulo', 'Titulo obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve estar entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor', 'Autor obrigatório').notEmpty();
+        req.assert('datanoticia', 'data obrigatório').notEmpty().isDate({ format: 'YYYY-MM-DD' });
+
+        var errors = req.validationErrors();
+
+        if(errors){
+            console.log(errors);
+            return;
+        }
+            
+
         var connection = app.config.dbcontext();
         var noticiasModel = new app.app.models.noticiasModel(connection);
-        
-        var noticia = req.body;
-
+    
         noticiasModel.cadastrarNoticia(noticia, function(error, result){
             res.redirect('/noticias');
         });
