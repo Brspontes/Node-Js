fe07860ab9a44138a81814e27b7230e4a38e1f17
Brspontes <brian.robert16@hotmail.com>
2020-12-02 20:11:37 -0300

    Finalizando camada de dados

--- Node-Avancado/src/data/usercases/AddAccount/db-add-account.spec.ts
+++ Node-Avancado/src/data/usercases/AddAccount/db-add-account.spec.ts
@@ -1,10 +1,14 @@
+import { AccountModel } from './../../../domain/models/account';
+import { AddAccountModel } from './../../../domain/usercases/add-account';
 import { rejects } from 'assert';
-import { Encrypter } from './../../protocols/encrypter';
+import { Encrypter } from '../../protocols/encrypter';
 import { DbAddAccount } from './db-add-account';
+import { AddAccountRepository } from '../../protocols/add-account-repository';
 
 interface SutTypes {
     sut: DbAddAccount,
-    encrypterStub: Encrypter
+    encrypterStub: Encrypter,
+    addAccountRepositoryStub: AddAccountRepository
 }
 
 const makeEncrypter = (): Encrypter => {
@@ -17,12 +21,30 @@ const makeEncrypter = (): Encrypter => {
     return new EncrypterStub()
 }
 
+const makeAddAccountRepository = (): AddAccountRepository => {
+    class AddAccountRepositoryStub implements AddAccountRepository {
+        async add (accountData: AddAccountModel): Promise<AccountModel> {
+            const fakeAccount = {
+                id: 'valid_id',
+                name: 'valida_name',
+                email: 'valida_email@mail.com',
+                password: 'hashed_password'
+            }
+            return new Promise(resolve => resolve(fakeAccount))
+        }
+    }
+
+    return new AddAccountRepositoryStub()
+}
+
 const makeSut = (): SutTypes => {
+    const addAccountRepositoryStub = makeAddAccountRepository()
     const encrypterStub = makeEncrypter ()
-    const sut = new DbAddAccount(encrypterStub)
+    const sut = new DbAddAccount(encrypterStub, addAccountRepositoryStub)
     return {
         sut,
-        encrypterStub
+        encrypterStub,
+        addAccountRepositoryStub
     }
 }
 
@@ -49,5 +71,21 @@ describe('DbAddAccount UseCase', () => {
         }
         const promise = sut.add(accountData)
         expect(promise).rejects.toThrow()
+    })
+
+    test('Should call AddAccountRepository with correct values', async () => {
+        const { sut, addAccountRepositoryStub } = makeSut()
+        const addSpy = jest.spyOn(addAccountRepositoryStub, 'add')
+        const accountData = {
+            name: 'valida_name',
+            email: 'valida_email@mail.com',
+            password: 'valid_password'
+        }
+        await sut.add(accountData)
+        expect(addSpy).toHaveBeenCalledWith({
+            name: 'valida_name',
+            email: 'valida_email@mail.com',
+            password: 'hashed_password'
+        })
     })
 })
--- Node-Avancado/src/data/usercases/AddAccount/db-add-account.ts
+++ Node-Avancado/src/data/usercases/AddAccount/db-add-account.ts
@@ -1,14 +1,20 @@
-import { Encrypter } from './../../protocols/encrypter'
+import { AddAccountRepository } from './../../protocols/add-account-repository';
+import { Encrypter } from '../../protocols/encrypter'
 import { AccountModel } from '../../../domain/models/account'
-import { AddAccount, AddAccountModel } from './../../../domain/usercases/add-account'
+import { AddAccount, AddAccountModel } from '../../../domain/usercases/add-account'
 
 export class DbAddAccount implements AddAccount{
     private readonly encrypter: Encrypter
+    private readonly addAccountRepository: AddAccountRepository
 
-    constructor(encrypter: Encrypter) { this.encrypter = encrypter }
+    constructor(encrypter: Encrypter, addAccountRepository: AddAccountRepository) { 
+        this.encrypter = encrypter 
+        this.addAccountRepository = addAccountRepository
+    }
 
-    async add(account: AddAccountModel): Promise<AccountModel> {
-        await this.encrypter.encrypt(account.password)
-        return new Promise(resolve => resolve(null))
+    async add(accountData: AddAccountModel): Promise<AccountModel> {
+        const hashed = await this.encrypter.encrypt(accountData.password)
+        const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashed }))
+        return new Promise(resolve => resolve(account))
     }
 }
