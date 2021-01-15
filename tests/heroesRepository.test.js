4e597cc10d549d80352bc714afff87654872a176
Brspontes <brian.robert16@hotmail.com>
2021-01-15 13:29:40 -0300

    atualizando packages

---
+++ tests/heroesRepository.test.js
@@ -0,0 +1,15 @@
+// const { query } = require('express')
+// const HeroesController = require('./../src/controllers/HeroesController')
+// const connection = require('./../src/repository/dbContext/connection')
+
+// jest.mock('connection', () => {
+//     async query () {
+
+//     }
+// })
+
+// describe("Teste Heroes Repository", () => {
+//     test('Obter Herois', async () => {
+//         jest.spyOn(connection, 'query')
+//     })
+// })    
